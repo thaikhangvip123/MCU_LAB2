aff824dcab0c2498edbea13614608551cb9e1425
DESKTOP-JK6HBSK\thaik <thaikhangvip123@gmail.com>
2024-11-02 09:03:35 +0700

    Init fix EN_Pin

--- MCU_Lab_2/Core/Src/main.c
+++ MCU_Lab_2/Core/Src/main.c
@@ -70,7 +70,7 @@ void display7SEG(int num) {
 	}
 }
 void clear() {
-	HAL_GPIO_WritePin(GPIOA, EN0_Pin | EN1_Pin, SET);
+	HAL_GPIO_WritePin(GPIOA, EN0_Pin | EN1_Pin | EN2_Pin | EN3_Pin, SET);
 //	HAL_GPIO_WritePin(GPIOB, SEG0_Pin | SEG1_Pin | SEG2_Pin |
 //					SEG3_Pin | SEG4_Pin | SEG5_Pin | SEG6_Pin, RESET);
 }
