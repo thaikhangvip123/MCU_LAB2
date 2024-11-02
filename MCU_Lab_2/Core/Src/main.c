9b2d0c72310c991465b0fbcdd3afd7ff5ce95830
DESKTOP-JK6HBSK\thaik <thaikhangvip123@gmail.com>
2024-11-02 09:16:09 +0700

    Init fix code ex7

--- MCU_Lab_2/Core/Src/main.c
+++ MCU_Lab_2/Core/Src/main.c
@@ -71,8 +71,8 @@ void display7SEG(int num) {
 }
 void clear() {
 	HAL_GPIO_WritePin(GPIOA, EN0_Pin | EN1_Pin | EN2_Pin | EN3_Pin, SET);
-//	HAL_GPIO_WritePin(GPIOB, SEG0_Pin | SEG1_Pin | SEG2_Pin |
-//					SEG3_Pin | SEG4_Pin | SEG5_Pin | SEG6_Pin, RESET);
+	HAL_GPIO_WritePin(GPIOB, SEG0_Pin | SEG1_Pin | SEG2_Pin |
+					SEG3_Pin | SEG4_Pin | SEG5_Pin | SEG6_Pin, RESET);
 }
 void enable(int pin) {
 	HAL_GPIO_WritePin(GPIOA, controllPin[pin], RESET);
